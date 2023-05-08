f5e7fbbf93a8d93a7ad6b6e27a7b3894c4071c47
menriqueandrade <56304628+menriqueandrade@users.noreply.github.com>
2023-05-07 21:41:06 -0500

    refactor

--- src/main/java/com/apispring/api_spring/repository/productos/ProductoRepositorio.java
+++ src/main/java/com/apispring/api_spring/repository/productos/ProductoRepositorio.java
@@ -1,11 +1,9 @@
 package com.apispring.api_spring.repository.productos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.apispring.api_spring.models.productos.Producto;
 
-@Repository
-public interface ProductoRepositorio extends JpaRepository<Producto,Long>{
-    
+public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
+
 }
